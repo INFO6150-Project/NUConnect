9928f26253c17efc70b4cf00457ddf92a6bb0ea4
Kshitij Kumar <kumar.ks@northeastern.edu>
2024-10-08 22:42:49 -0400

    project_template_creation

---
+++ client/script.js
@@ -0,0 +1,9 @@
+// let url = "level3.html"; <-- following line shows how to get this from url
+window.onload = function (){
+    let currentItem = document.getElementByClassName("items").find("[href$='" + url + "']");
+let path = "home";
+$(currentItem.parents("li").get().reverse()).each(function () {
+    path += "/" + this.children("a").innerText;
+});
+document.getElementByClassName("bredcrumb").innerHTML = path;
+}
